ad248f2c582e6d8e0cd87a948fa63bd43488da8b
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-22 01:10:44 +0530

    Add NLP endpoint to retrieve and process text data from files ✅

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -8,3 +8,34 @@ import { nlpService } from "../Controllers/NlpController.js";
 export const router = Router();
 
 router.post("/upload", upload.single("file"), UploadController.upload, PdfParsing.parsePdf);
+
+router.get('/nlp/:dataId', async(req:any,res) => {
+    const {dataId} = req.params;
+    const userId = req.userId;
+    try {
+
+        const data = await prisma.file.findUnique({
+            where:{
+                id: dataId,
+                userId: userId
+            }, 
+            include:{
+                user: true
+            }
+        });
+
+        const text = data?.text;
+        console.log(text);
+        const nlpResult = await nlpService(text!);
+        
+        res.status(200).json({
+            message: "NLP Service success",
+            data: nlpResult
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error
+        })
+    }
+})
