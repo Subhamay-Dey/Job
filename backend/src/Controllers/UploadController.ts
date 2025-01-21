2df9165ff5fccdc0865ca901e9cf5b70d6976949
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-21 19:20:56 +0530

    UploadController api code done ✅

---
+++ backend/src/Controllers/UploadController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import fs from "fs";
+import { supabase } from "../supabase/supabaseClient.js";
+
+interface File {
+  buffer: Buffer;
+  mimetype: string;
+  originalname: string;
+  path: string;
+}
+
+class UploadController {
+  static async upload(req: Request, res: Response) {
+    try {
+      const file: File = req.file;
+      const filename = `${Date.now()}-${file.originalname}`;
+      const filePath = file.path;
+
+      const fileBuffer = fs.readFileSync(filePath);
+
+      const { data, error: uploadError } = await supabase.storage
+        .from("pdf")
+        .upload(filename, file.buffer, {
+          contentType: file.mimetype,
+          upsert: true,
+        });
+
+      if (uploadError) {
+        console.log("Supabase Upload error:", uploadError.message);
+        return res
+          .status(500)
+          .json({ error: "Failed to upload file to Supabase" });
+      }
+
+      const { data:publicURL, error: publicURLError } = supabase.storage
+        .from("pdf")
+        .getPublicUrl(`upload/${filename}`);
+
+        if (publicURLError) {
+          console.log("Supabase Public URL error:", publicURLError.message);
+          return res
+            .status(500)
+            .json({ error: "Failed to get public URL from Supabase" });
+        }
+
+        fs.unlinkSync(filePath);
+
+        return res.status(200).json({message: "File uploaded successfully", fileUrl: publicURL });
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  }
+}
