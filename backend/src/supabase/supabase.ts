9130e6d68883dae99452ac869cf107a31be7b2de
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-21 19:38:44 +0530

    Added dotenv import and configuration to supabase.ts ✅

--- backend/src/supabase/supabase.ts
+++ backend/src/supabase/supabase.ts
@@ -1,6 +1,10 @@
-// src/lib/supabase.js
 import { createClient } from '@supabase/supabase-js'
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
