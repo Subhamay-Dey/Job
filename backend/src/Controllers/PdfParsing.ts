13b6d624ca18dc822ec9d616ba2579f5bf9c0a93
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-21 23:00:58 +0530

    `feat: Update dependencies and configuration for TypeScript project`
    `Changes include:
    
    - Updated package-lock.json and package.json to reflect new dependencies
    - Updated tsconfig.json to change module resolution and other settings
    - Added support for parsing PDF files in the

--- backend/src/Controllers/PdfParsing.ts
+++ backend/src/Controllers/PdfParsing.ts
@@ -1,12 +1,13 @@
-import pdf from 'pdf-parse';
+import pdf from "pdf-parse";
 
 class PdfParsing {
   static async parsePdf(pdfBuffer: Buffer): Promise<string> {
     try {
-        const data = await pdf(pdfBuffer);
-        return data.text;
+      const data = await pdf(pdfBuffer);
+      return data.text;
     } catch (error) {
-        console.log(error);
+      console.error("Error parsing PDF:", error);
+      throw error;
     }
   }
 }
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import UploadController from "../Controllers/UploadController.js";
 import upload from "../middleware/multerConfig.js";
+import PdfParsing from "../Controllers/PdfParsing.js";
 
 export const router = Router();
 
 router.post("/upload", upload.single("file"), UploadController.upload)
+
+router.post("/parse", upload.single("file"), PdfParsing.parsePdf); 
