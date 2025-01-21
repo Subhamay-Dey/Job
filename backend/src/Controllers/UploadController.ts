d2b1dc9cc7061f6a35d4a81def0759a576f87b6f
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-22 00:52:50 +0530

    updated File model and UploadController to match schema changes ✅

--- backend/src/Controllers/UploadController.ts
+++ backend/src/Controllers/UploadController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import fs from "fs";
 import { supabase } from "../supabase/supabase.js";
 import PdfParsing from "./PdfParsing.js";
+import prisma from "../prisma/index.js";
 
 class UploadController {
   static async upload(req: Request, res: Response) {
@@ -43,6 +44,16 @@ class UploadController {
         orinalName: file.originalname,
       }
       const Parsepdf = await PdfParsing.parsePdf(req.file?.buffer!)
+
+      const Data = await prisma.file.create({
+        data: {
+          text: Parsepdf,
+          file : upload,
+          user: req.user?.id
+        }
+      })
+
+      res.status(200).json({ data: Data ,message: "File uploaded successfully"});
 
     } catch (error) {
       console.error("Unexpected Error:", error);
