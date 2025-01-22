3055ff523f1eeba67fcbe2d6f45e5668ff8d33e2
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-22 15:20:51 +0530

    shadcn added ✅

--- frontend/src/auth/SignIn.tsx
+++ frontend/src/auth/SignIn.tsx
@@ -23,7 +23,7 @@ export default function SignIn() {
     setIsLoading(true)
 
     try {
-      const response = await fetch("https://formai-mwwx.onrender.com/api/v1/signin", {
+      const response = await fetch("https://localhost:8000/api/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
