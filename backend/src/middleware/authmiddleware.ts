8ed251aed700cb6964ab6a064d4ae52f7384f4ba
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-22 21:30:52 +0530

    deployment change ✅

--- backend/src/middleware/authmiddleware.ts
+++ backend/src/middleware/authmiddleware.ts
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
 
-export const authMiddleware = (req:any, res:Response, next:NextFunction) => {
+export const authMiddleware = (req:any, res:any, next:any) => {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) return res.status(401).json({ message: "No token provided" });
   
