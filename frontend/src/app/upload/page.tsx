418fee2f83a1e93d779386e9254c161a030f2211
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-22 15:55:01 +0530

    added upload page component

---
+++ frontend/src/app/upload/page.tsx
@@ -0,0 +1,10 @@
+import Upload from '@/main/Upload'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='min-h-screen w-full flex flex-col justify-center items-center'>
+        <Upload/>
+    </div>
+  )
+}
