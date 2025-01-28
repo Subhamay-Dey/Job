43519c98c84d9874c347ebe565770ea5796baf63
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-28 18:45:41 +0530

    add pdf-parse type definitions and refactor pdf parsing to class method ✅

--- server/src/Controllers/PdfParsing.ts
+++ server/src/Controllers/PdfParsing.ts
@@ -1,10 +1,12 @@
 import pdf from 'pdf-parse';
 
-export async function pdfParsing(buffer:Buffer) {
-    try {
-        const data = await pdf(buffer);
-        return data.text;
-    } catch (error) {
-        console.log('Pdf parsing error:', error);
+class PdfParsing {
+    static async parse(buffer: Buffer) {
+        try {
+            const data = await pdf(buffer);
+            return data.text;
+        } catch (error) {
+            console.log('Pdf parsing error:', error);
+        }
     }
 }
