f17d0309fdd62627c6a4d9c1d1c5ce1c89f871c0
Subhamay-Dey <ncdey1966@gmail.com>
2025-01-28 18:37:18 +0530

    feat: implement file upload functionality using Supabase storage in class architecture ✅

---
+++ server/src/Controllers/FileUpload.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { supabase } from "../supabase/supabase.js";
+
+interface File {
+    buffer: Buffer;
+    originalname: string;
+    mimetype: string;
+  }
+
+class FileUpload {
+    static async upload(file:File) {
+        try {
+            const filename = `${Date.now()}-${file.originalname}`;
+            const buffer = file.buffer;
+            const originalname = file.originalname;
+            const mimetype = file.mimetype;
+
+            const {error} = await supabase.storage.from("Pdf").upload(
+            filename,
+            buffer,
+            {contentType: mimetype, cacheControl: "3600",});
+
+            if(error) {
+                console.error('Supabase Error:', {  
+                    message: error.message
+                });
+                throw error;
+            }
+
+            return {
+                filename: filename,
+                originalname: originalname,
+            }
+
+        } catch (error) {
+            console.error(error.message);
+            throw new Error('Failed to upload file to storage');  
+        }
+
+    }
+
+    static async url(filename:string) {
+        try {
+            const {data: { publicUrl }} = await supabase.storage
+            .from("Pdf")
+            .getPublicUrl(filename);
+
+            return {
+                url : publicUrl
+            }
+
+        } catch (error) {
+            console.error(error.message);
+            throw new Error("Failed to get public URL");
+        }
+    }
+}
+
+export default FileUpload;
